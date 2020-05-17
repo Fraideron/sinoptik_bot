2d366c2574e77556212cf4bbe159ca363b2f3c6e
valeriy <jiafen24@gmail.com>
2020-05-17 23:36:41 +0300

    fix message response timeout

--- commands/onMessage.js
+++ commands/onMessage.js
@@ -5,16 +5,13 @@ const onMessage = (bot) => {
         const city = ctx && 
                      ctx.update && 
                      ctx.update.message && 
-                     ctx.update.message.text || '';
+                     ctx.update.message.text;
 
         const message = await ctx.reply('Генерируем описание...)');                                           
         const sinoptik = new Sinoptik(city);
-        sinoptik.getDescription().then((weather) => {
-            await ctx.deleteMessage(message.message_id);
-            await ctx.reply(weather || 'Не можем найти указаный город. Попробуйте, ещё раз...');
-        }).catch((err) => {
-            await ctx.reply(weather || 'Не можем найти указаный город. Попробуйте, ещё раз...');
-        });
+        const weather = await sinoptik.getDescription()
+        await ctx.deleteMessage(message.message_id);
+        await ctx.reply(weather || 'Не можем найти указаный город. Попробуйте, ещё раз...');
     })
 }
 module.exports = onMessage;
